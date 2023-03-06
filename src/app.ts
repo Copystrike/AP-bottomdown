6491b679c2bcd957e86eabf0247fe0640c8bb27e
Nick <verbraeckennick@gmail.com>
2023-03-06 18:15:54 +0000

    refactor: Moved everything about the website to /src

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,13 @@ import express, { Application } from "express";
 import { PORT_NUMBER } from "./config/debug";
 
 const app: Application = express();
-app.use(express.json());
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", "src/pages");
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+app.use(express.static("src/public")); // To serve static files such as images, CSS files, and JavaScript files
+app.set("view engine", "ejs"); // To set the view engine
+app.set("views", "src/pages"); // To set the views directory
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Fortnite" });
 });
 
 // Starting the server
