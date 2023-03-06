39b88cd4b3200340b080ab6177ca6ef9cf291d57
Buytaert Dries (s113387) <dries.buytaert@student.ap.be>
2023-03-06 16:09:28 +0100

    commit

--- landingPage.js
+++ landingPage.js
@@ -1,13 +1,1 @@
-// create a button element
-const button = document.createElement("button");
-
-// set the button text
-button.textContent = "Click me!";
-
-// add an event listener to the button
-button.addEventListener("click", () => {
-  console.log("Button clicked!");
-});
-
-// append the button to the HTML body
-document.body.appendChild(button);
+console.log("HHALLOOO");
