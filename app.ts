313526236a44a76597facd5033b45401f3932e1f
Copystrike <verbraeckennick@gmail.com>
2023-03-06 16:31:51 +0100

    Some changes

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
 import express, { Application } from "express";
-import http from "http";
 import { PORT_NUMBER } from "./config/debug";
 
 const app: Application = express();
-const server: http.Server = http.createServer(app);
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pages/index", {
+
+  });
+});
 
 // Starting the server
-server.listen(PORT_NUMBER, () => {
+app.listen(PORT_NUMBER, () => {
   console.log(`SERVER RUNNING ON ${PORT_NUMBER}`);
 });
