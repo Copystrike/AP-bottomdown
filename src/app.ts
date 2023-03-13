e8ec1de339899ad07fbf7ef3b94be5bf0b1820a4
Mourad El Harchaoui <99733818+mouradelh@users.noreply.github.com>
2023-03-13 15:02:24 +0100

    avatarchooser page

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import { PORT_NUMBER } from "./config/debug";
+const axios = require('axios');
 
 const app: Application = express();
 app.use(express.json()); // To parse the incoming requests with JSON payloads
@@ -7,9 +8,24 @@ app.use(express.static("src/public")); // To serve static files such as images, 
 app.set("view engine", "ejs"); // To set the view engine
 app.set("views", "src/pages"); // To set the views directory
 
-app.get("/", (req, res) => {
+
+app.get('/', async (req, res) => {
   res.render("index");
 });
+
+app.get('/avatar', async (req, res) => {
+  try {
+    const response = await axios.get('https://fortnite-api.theapinetwork.com/items/list');
+    const items = response.data.data; // get the first 4 items from the response
+    console.log(items);
+    res.render('avatar', { items });
+  } catch (error) {
+    console.error(error);
+    res.send('Error!');
+  }
+});
+
+
 
 // Starting the server
 app.listen(PORT_NUMBER, () => {
