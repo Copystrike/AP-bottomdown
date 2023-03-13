25d6b25f270529b407a6c895c0d9a51c380b1c6d
Copystrike <verbraeckennick@gmail.com>
2023-03-13 17:02:48 +0100

    Updated avatar api

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from "express";
+import express, { Application, response } from "express";
 import { PORT_NUMBER } from "./config/debug";
-const axios = require('axios');
+import axios from "axios";
+import { FortniteItem } from "./types/fortnite";
 
 const app: Application = express();
 app.use(express.json()); // To parse the incoming requests with JSON payloads
@@ -8,26 +9,38 @@ app.use(express.static("src/public")); // To serve static files such as images, 
 app.set("view engine", "ejs"); // To set the view engine
 app.set("views", "src/pages"); // To set the views directory
 
-
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index");
 });
 
-app.get('/avatar', async (req, res) => {
+app.get("/avatar", async (req, res) => {
+  // const response = await axios.get<FortniteItem>("https://fortnite-api.theapinetwork.com/items/list");
+  const axiosResponse = await axios.get<FortniteItem>("http://127.0.0.1:3000/fortnite-items.json");
+  const response = axiosResponse.data;
+  const outfitItems = response.data.filter((item) => item.item.type === "outfit");
+
+  // randomItems
+  const randomItems = [];
+  for (let i = 0; i < 10; i++) {
+    const randomItemIndex = randomConstraint(0, outfitItems.length - 1);
+    const item = outfitItems[randomItemIndex];
+    randomItems.push(item);
+  }
+
   try {
-    const response = await axios.get('https://fortnite-api.theapinetwork.com/items/list');
-    const items = response.data.data; // get the first 4 items from the response
-    console.log(items);
-    res.render('avatar', { items });
+    res.render("avatar", { items: randomItems });
   } catch (error) {
     console.error(error);
-    res.send('Error!');
+    res.send("Error!");
   }
 });
 
-
+// random constaint function
+const randomConstraint = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 // Starting the server
 app.listen(PORT_NUMBER, () => {
-  console.log(`SERVER RUNNING ON ${PORT_NUMBER}`);
+  console.log(`SERVER RUNNING ON http://127.0.0.1:${PORT_NUMBER}/`);
 });
