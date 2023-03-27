d85a72e029a249437e3ef6d22bdc39035471d025
Copystrike <verbraeckennick@gmail.com>
2023-03-27 12:00:02 +0200

    Changes

--- src/app.ts
+++ src/app.ts
@@ -16,35 +16,12 @@ app.get("/", async (req, res) => {
   res.render("index");
 });
 
-axios.get<FortniteItem>(FORTNITE_API_URL).then((axiosResponse) => {
-  app.get("/avatar", async (req, res) => {
-    const response = axiosResponse.data;
-    const outfitItems = response.data.filter((item) => item.item.type === "outfit");
-
-    // randomItems
-    const randomItems: MetaData[] = [];
-    for (let i = 0; i < 10; i++) {
-      const randomItemIndex = randomConstraint(0, outfitItems.length - 1);
-      const item = outfitItems[randomItemIndex];
-
-      if (randomItems.includes(item)) {
-        i--;
-        continue;
-      }
-      randomItems.push(item);
-    }
-
-    try {
-      res.render("avatar", { items: randomItems });
-    } catch (error) {
-      console.error(error);
-      res.send("Error!");
-    }
-  });
-});
-
 app.get("/nogame", (req, res) => {
   res.render("nogame");
+});
+
+app.get("/favoriete", (req, res) => {
+  res.render("favoriete");
 });
 
 axios.get<FortniteItem>(FORTNITE_API_URL).then((axiosResponse) => {
