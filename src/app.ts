75bf3c463343d9cbeeea345bfec7f1f189f66ced
Copystrike <verbraeckennick@gmail.com>
2023-03-27 10:16:08 +0200

    Disable page caching for development purposes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application } from "express";
 import { PORT_NUMBER } from "./config/debug";
+import { IS_DEV } from "./constants";
 
 const app: Application = express();
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 app.use(express.static("src/public")); // To serve static files such as images, CSS files, and JavaScript files
 app.set("view engine", "ejs"); // To set the view engine
 app.set("views", "src/pages"); // To set the views directory
+app.disable('view cache'); // disable view cache
 
 app.get("/", (req, res) => {
   res.render("index");
