e1013085800dcc9a33640785cfea29a88ac36543
Buytaert Dries (s113387) <dries.buytaert@student.ap.be>
2023-03-28 10:29:14 +0200

    scrollspeed aangepast

--- src/public/js/landing-page.js
+++ src/public/js/landing-page.js
@@ -3,7 +3,7 @@ const scrollContainer = document.querySelector('.games');
 scrollContainer.addEventListener('mousemove', (event) => {
   
 
-  const scrollSpeed = 20;
+  const scrollSpeed = 50;
   const direction = event.clientX < window.innerWidth / 2 ? -1 : 1;
 
   if (event.clientX < window.innerWidth * 0.1 || event.clientX > window.innerWidth * 0.9) {
