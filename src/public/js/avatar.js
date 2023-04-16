619ece502834fb88315469ed677dcafcca05d824
Nick <verbraeckennick@gmail.com>
2023-04-16 11:53:09 +0000

    Info about css files

--- src/public/js/avatar.js
+++ src/public/js/avatar.js
@@ -5,12 +5,15 @@ function setAvatar(avatarUrl) {
   avatarContainer.innerHTML = "";
   avatarContainer.appendChild(avatar);
 }
+
 myButton.addEventListener("click", function () {
   myPopup.classList.add("show");
 });
+
 closePopup.addEventListener("click", function () {
   myPopup.classList.remove("show");
 });
+
 window.addEventListener("click", function (event) {
   if (event.target == myPopup) {
     myPopup.classList.remove("show");
