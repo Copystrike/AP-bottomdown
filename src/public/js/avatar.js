49a75ab75f28c3acbbd0419b068fd3c107c4970c
Nick <verbraeckennick@gmail.com>
2023-04-16 11:46:16 +0000

    Made some changes to make avatar page work

--- src/public/js/avatar.js
+++ src/public/js/avatar.js
@@ -5,3 +5,14 @@ function setAvatar(avatarUrl) {
   avatarContainer.innerHTML = "";
   avatarContainer.appendChild(avatar);
 }
+myButton.addEventListener("click", function () {
+  myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+  myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+  if (event.target == myPopup) {
+    myPopup.classList.remove("show");
+  }
+});
--- src/public/js/landing-page.js
+++ src/public/js/landing-page.js
@@ -1,17 +1,10 @@
-const scrollContainer = document.querySelector('.games');
+const scrollContainer = document.querySelector(".games");
 
-scrollContainer.addEventListener('mousemove', (event) => {
-  
-
+scrollContainer.addEventListener("mousemove", (event) => {
   const scrollSpeed = 50;
   const direction = event.clientX < window.innerWidth / 2 ? -1 : 1;
 
   if (event.clientX < window.innerWidth * 0.1 || event.clientX > window.innerWidth * 0.9) {
-
     scrollContainer.scrollLeft += direction * scrollSpeed;
   }
-  
-
-  
-
 });
