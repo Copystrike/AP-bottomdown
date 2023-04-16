64d158ebc7d5fa9eeeff8865c7741add42a9934b
Nick <verbraeckennick@gmail.com>
2023-04-16 13:06:57 +0000

    Finished modal

--- src/public/js/functies/modal.js
+++ src/public/js/functies/modal.js
@@ -1,21 +1,58 @@
-var modal = document.getElementById("modal");
-
-var avatarButtons = document.getElementsByClassName("modal-btn");
-
-var closeModal = document.getElementsByClassName("modal-close")[0];
-
-Array.from(avatarButtons).forEach((btn) => {
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
-});
-
-closeModal.onclick = function () {
-  modal.style.display = "none";
+window.onload = function () {
+  injectModal();
 };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function injectModal() {
+  const modal = document.getElementById("modal");
+  if (modal) {
+    document.getElementById("modal").outerHTML = `
+          <div id="modal" class="modal">
+            <div class="modal-content">
+              <div class="modal-header">
+                <span class="modal-close">&times;</span>
+                <h2>Loading...</h2>
+              </div>
+              <div class="modal-body">
+                <p>Loading...</p>
+              </div>
+              <div class="modal-footer">
+                <h3>Loading...</h3>
+              </div>
+            </div>
+          </div>
+      `;
+    loadModel();
   }
-};
+}
+
+function loadModel() {
+  var modal = document.getElementById("modal");
+  var modalButtons = document.getElementsByClassName("modal-btn");
+  var closeModal = document.getElementsByClassName("modal-close")[0];
+
+  const modelTitle = document.getElementsByClassName("modal-header")[0];
+  const modelBody = document.getElementsByClassName("modal-body")[0];
+  const modelFooter = document.getElementsByClassName("modal-footer")[0];
+
+  const onModelOpen = (button) => {
+    modal.style.display = "block";
+    // Zal 'modelOpen' function uitvoeren, zorg er voor dat deze function bestaat op jouw pagina
+    // Deze functie zal ook de data van de button meesturen en model elementen zo dat je deze kan aanpassen
+    modelOpen(button, { modelTitle, modelBody, modelFooter });
+  };
+
+  const onModelClose = () => {
+    modal.style.display = "none";
+    modelClose(); // Zal deze function uitvoeren, zorg er voor dat deze function bestaat op de jouw pagina
+  };
+
+  Array.from(modalButtons).forEach((button) => (button.onclick = () => onModelOpen(button)));
+
+  closeModal.onclick = () => onModelClose();
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      onModelClose();
+    }
+  };
+}
