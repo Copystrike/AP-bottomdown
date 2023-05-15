ad4703158d53f11965bb103c8106dd7b475c5db7
Mourad El Harchaoui <99733818+mouradelh@users.noreply.github.com>
2023-05-15 14:33:27 +0200

    mongodb

--- src/public/js/favoriete.js
+++ src/public/js/favoriete.js
@@ -1,12 +1,12 @@
-var button = document.getElementById("btn btn-secondary"),
+var button = document.getElementById("win"),
   count = 0;
 button.onclick = function() {
   count += 1;
   button.innerHTML = "Wins: " + count;
 };
-var button = document.getElementById("btn btn-danger"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Loss: " + count;
+var button1 = document.getElementById("lose"),
+  counter = 0;
+button1.onclick = function() {
+  counter += 1;
+  button1.innerHTML = "Loss: " + count;
 };
