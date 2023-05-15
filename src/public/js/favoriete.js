80525acd84203af1183905577bca5ec76496233a
souhaib beladel <souhaib.beladel@ap.be>
2023-05-15 14:48:55 +0200

    favoriete pagina

--- src/public/js/favoriete.js
+++ src/public/js/favoriete.js
@@ -1,12 +1,9 @@
-var button = document.getElementById("btn btn-secondary"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Wins: " + count;
-};
-var button = document.getElementById("btn btn-danger"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-  button.innerHTML = "Loss: " + count;
-};
+
+
+let popup = document.getElementById("popup");
+      function openPopup(){
+        popup.classList.add("open-popup");
+      }
+      function closePopup(){
+        popup.classList.remove("open-popup");
+      };
